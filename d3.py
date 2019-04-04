52e30691b09fdc5a4ba869ac4faf42159c1eb557
Karan Bhalani <karanbhalanii@gmail.com>
2019-04-04 17:51:47 +0530

    divisible by 3 program

---
+++ d3.py
@@ -0,0 +1,9 @@
+x = int(input())
+if(x%3==1):
+	print("remainder is 1")
+elif(x%3==2):
+	print("remainder is 2")
+elif(x%3==0):
+	print("number is divisible by 3")
+else:
+	print("input kya daala chaman?")
