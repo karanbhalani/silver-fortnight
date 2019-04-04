68892a597f01fa84a67d308cd42b34db1ab89e47
Karan Bhalani <karanbhalanii@gmail.com>
2019-04-04 19:00:27 +0530

    functions and classes

---
+++ class.py
@@ -0,0 +1,13 @@
+class Calculator():
+	def add(a,b):
+		print(a+b)
+	def sub(a,b):
+		print(a-b)
+	def mul(a,b):
+		print(a*b)
+	def div(a,b):
+		print(a/b)	
+a=int(input()) 
+b=int(input())
+Calculator.add(a,b)
+Calculator.sub(a,b)
---
+++ func.py
@@ -0,0 +1,4 @@
+def add(a,b):
+	return a + b
+x = add(10,20)
+print(x)
